1c4bdeb8ad4e37669aa44ff95aa570b4944aeaab
zhangzhengkun <hzzzk@tairanchina.com>
2019-04-19 20:00:40 +0800

    add caller

---
+++ app/Caller/AliCaller.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Caller;
+
+class AliCaller extends HttpCaller
+{
+
+}
---
+++ app/Caller/WxCaller.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Caller;
+
+/**
+ * 微信接口调用器
+ *
+ * @author zhangzhengkun
+ */
+class WxCaller extends HttpCaller
+{
+
+}
