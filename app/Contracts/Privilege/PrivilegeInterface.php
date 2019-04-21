33710068d071d4d876af22bf4a528b2cac0beeb3
googol24 <sa514132@mail.ustc.edu.cn>
2019-04-21 12:16:36 +0800

    update contracts

--- app/Contracts/Privilege/PrivilegeInterface.php
+++ app/Contracts/Privilege/PrivilegeInterface.php
@@ -12,11 +12,21 @@ interface PrivilegeInterface
      * 创建角色
      *
      * @param string $roleName
-     * @param string $privilegeList
+     * @param array $privilegeList
      *
      * @return int
      */
     public function createRole($roleName, $privilegeList);
+
+    /**
+     * 设置角色的禁启用状态
+     *
+     * @param int $roleId
+     * @param int $status
+     *
+     * @return int
+     */
+    public function setRoleStatus($roleId, $status);
 
     /**
      * 查看角色详情
