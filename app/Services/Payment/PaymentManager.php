ef7b1e3a30abd71e03162f2fb7c0e8732a5cb346
wangyingqian <891699279@qq.com>
2019-05-05 10:49:36 +0800

    支付

--- app/Services/Payment/PaymentManager.php
+++ app/Services/Payment/PaymentManager.php
@@ -1,5 +1,16 @@
 <?php
 namespace App\Services\Payment;
 
+use App\Daos\PaymentDao;
+
 class PaymentManager
-{}
+{
+    protected $paymentDao;
+
+    public function __construct(PaymentDao $paymentDao)
+    {
+        $this->paymentDao = $paymentDao;
+    }
+
+
+}
