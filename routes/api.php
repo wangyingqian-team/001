49086271f79a1fe70c4dc018d1a62017172ddd0c
wangyingqian <891699279@qq.com>
2019-05-22 14:45:36 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -43,3 +43,7 @@ Route::namespace('Payment')->prefix('payment')->group(function () {
 
     require_once  __DIR__ . '/api/payment.php';
 });
+
+Route::get('/',function (){
+   return 'Hello Dine';
+});
